e511a8618286ee221d06f90b761a0735a00fa0a1
Gustavo Londa <gustavolonda@gmail.com>
2022-02-17 21:21:14 -0500

    Create Services

---
+++ user-service/src/main/java/com/iverno/gus/userservice/registration/phone/application/service/PhoneService.java
@@ -0,0 +1,44 @@
+package com.iverno.gus.userservice.registration.phone.application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import com.iverno.gus.commonservice.endpoint.application.service.EndPointServiceImpl;
+import com.iverno.gus.commonservice.endpoint.business.object.StatusDomain;
+
+import static com.iverno.gus.commonservice.endpoint.util.DateUtil.now;
+import com.iverno.gus.userservice.registration.phone.domain.entity.PhoneEntity;
+import com.iverno.gus.userservice.registration.phone.domain.repository.PhoneRepository;
+
+@Service
+@Qualifier("phoneService")
+public class PhoneService extends EndPointServiceImpl<PhoneEntity, String> {
+	@Autowired
+	PhoneRepository repository;
+	@Override
+	public JpaRepository<PhoneEntity, String> getDao() {
+		// TODO Auto-generated method stub
+		return this.repository;
+	}
+
+	@Override
+	public PhoneEntity statusChangeDelete(PhoneEntity entity) {
+		entity.setStatus(StatusDomain.DELETE);
+		return entity;
+	}
+
+	@Override
+	public PhoneEntity runCreate(PhoneEntity entity) {
+		entity.setCreateDate(now());
+		return entity;
+	}
+
+	@Override
+	public PhoneEntity runUpdate(PhoneEntity entity) {
+		entity.setCreateUpdate(now());
+		return entity;
+	}
+
+}
---
+++ user-service/src/main/java/com/iverno/gus/userservice/registration/user/application/service/UserService.java
@@ -0,0 +1,48 @@
+package com.iverno.gus.userservice.registration.user.application.service;
+
+import static com.iverno.gus.commonservice.endpoint.util.DateUtil.now;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import com.iverno.gus.commonservice.endpoint.application.service.EndPointServiceImpl;
+import com.iverno.gus.commonservice.endpoint.business.object.StatusDomain;
+import com.iverno.gus.userservice.registration.user.domain.entity.UserEntity;
+import com.iverno.gus.userservice.registration.user.domain.repository.UserRepository;
+
+@Service
+@Qualifier("userService")
+public class UserService  extends EndPointServiceImpl<UserEntity, String> {
+	@Autowired
+	UserRepository repository;
+	
+	@Override
+	public JpaRepository<UserEntity, String> getDao() {
+		// TODO Auto-generated method stub
+		return this.repository;
+	}
+
+	@Override
+	public UserEntity statusChangeDelete(UserEntity entity) {
+		entity.setStatus(StatusDomain.DELETE);
+		return this.update(entity);
+	}
+
+	@Override
+	public UserEntity runCreate(UserEntity entity) {
+		entity.setCreateDate(now());
+		return entity;
+	}
+
+	@Override
+	public UserEntity runUpdate(UserEntity entity) {
+		entity.setCreateUpdate(now());
+		return entity;
+	}
+
+	
+
+
+}
